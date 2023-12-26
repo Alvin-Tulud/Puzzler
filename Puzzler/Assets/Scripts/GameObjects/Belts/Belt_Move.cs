2d77373ea8676e7a9f2955278d6c0eafdfd2b800
Alvin-Tulud <alvintulud@gmail.com>
2023-12-25 19:39:27 -0800

    belt balls

---
+++ Puzzler/Assets/Scripts/GameObjects/Belts/Belt_Move.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Belt_Move : MonoBehaviour
+{
+    private Transform   transformBelt;
+    private float       directionBelt;
+
+    private Transform   thingMoving;
+    private Vector3     thingMovingInitialPosition;
+    private Vector3     thingMovingCurrentPosition;
+    public  int         speedBelt;
+    private float       time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transformBelt = this.gameObject.GetComponent<Transform>();
+        directionBelt = transform.right.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (thingMoving != null) // check if ball on belt
+        {
+            thingMoving.rotation = Quaternion.Euler(0f, 0f, directionBelt);
+            if (time <= speedBelt)
+            {
+                thingMovingCurrentPosition.x = Mathf.Lerp(thingMovingInitialPosition.x, transformBelt.right.x, time / speedBelt);
+                thingMovingCurrentPosition.y = Mathf.Lerp(thingMovingInitialPosition.y, transformBelt.right.y, time / speedBelt);
+
+                thingMoving.position = new Vector3(thingMovingCurrentPosition.x, thingMovingCurrentPosition.y, 0f);
+
+                time++;
+                Debug.Log("(" + thingMovingCurrentPosition.x + ", " + thingMovingCurrentPosition.y + ") " + time);
+            }
+            else //stop moving ball
+            {
+                thingMoving = null;
+                time = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ball"))
+        {
+            thingMoving = collision.transform;
+            thingMovingInitialPosition = collision.transform.position;
+            Debug.Log(collision.tag);
+        }
+    }
+}
