1a77d114309c1bae057ff2bbbeddc37354967981
alee84sf <alee84@sfsu.edu>
2023-12-26 01:51:18 -0800

    working on turn director/counter

--- Puzzler/Assets/Scripts/GameObjects/Turn_Clicker.cs
+++ Puzzler/Assets/Scripts/GameObjects/Turn_Clicker.cs
@@ -1,29 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Turn_Clicker : MonoBehaviour
 {
 
     public int turnCounter; //Counts the number of elapsed turns, updates to this tell the factory to move
-    public Button clickerButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        Button btn = clickerButton.GetComponent<Button>();
-        btn.onClick.AddListener(IncrementOnClick);
-    }
-
-    void IncrementOnClick()
-    {
-
+        turnCounter = 0;
+        Debug.Log("Turn counter initialized and set  to 0 STUPID FREAKING KEYBOARD");
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        if(Input.GetKeyDown("p"))
+        {
+            Debug.Log("P pressed");
+            Increment();
+        }
         
+    }
+
+    //ADD A FIXED UPDATE
+
+    public void Increment()
+    {
+        turnCounter++; //increment the counter
+        //notify observers of the change
+        Debug.Log("INCREMENTING, counter is now " + turnCounter);
     }
 }
