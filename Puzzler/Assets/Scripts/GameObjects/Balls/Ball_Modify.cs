46ffac1fd24960fece87ab01a00c9c1bfe3563f6
Alvin-Tulud <alvintulud@gmail.com>
2024-01-01 20:03:53 -0800

    start goal script

--- Puzzler/Assets/Scripts/GameObjects/Balls/Ball_Modify.cs
+++ Puzzler/Assets/Scripts/GameObjects/Balls/Ball_Modify.cs
@@ -10,14 +10,24 @@ public class Ball_Modify : MonoBehaviour
     void Start()
     {
         modifications = new List<string>();
-        modifications.Add("");//color i0
-        modifications.Add("");//temperature i1
-        modifications.Add("");//idk i2 etc. etc. etc.
+    }
+
+    public void addMod(string mod)
+    {
+        if (modifications.Count < 4)
+        {
+            modifications.Add(mod);
+        }
     }
 
     public void setMod(int index, string mod)//changes modification on set index
     {
         modifications[index] = mod;
+    }
+
+    public void setModList(List<string> modifications)
+    {
+        this.modifications = modifications;
     }
 
     public List<string> getMod() { return modifications; }//returns lists to check for win condition
