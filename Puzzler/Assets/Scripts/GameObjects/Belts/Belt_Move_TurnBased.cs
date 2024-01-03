7570491e86fe79924ecef34f9fc2b215dac84d78
alee84sf <alee84@sfsu.edu>
2024-01-03 03:52:00 -0800

    beginning work on turnbased belt's movement

--- Puzzler/Assets/Scripts/GameObjects/Belts/Belt_Move_TurnBased.cs
+++ Puzzler/Assets/Scripts/GameObjects/Belts/Belt_Move_TurnBased.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Belt_Move_TurnBased : MonoBehaviour, Tile_Interface
 {
+    private Transform transformBelt;
+
+    private List<Transform> thingsMoving;
+    private List<Vector3> thingsMovingInitialPosition;
+    private Vector3 thingMovingCurrentPosition;
+    public int speedBelt;
+    private List<float> times;
+    private bool spotfound;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transformBelt = this.gameObject.GetComponent<Transform>();
+
+        thingsMoving = new List<Transform>();
+        thingsMovingInitialPosition = new List<Vector3>();
+        times = new List<float>();
+
+        spotfound = false;
     }
 
     // Update is called once per frame
@@ -16,14 +32,80 @@ public class Belt_Move_TurnBased : MonoBehaviour, Tile_Interface
         
     }
 
+    public IEnumerator moveThing()
+    {
+        float initialTime = Time.fixedDeltaTime;
+        float currentTime = Time.fixedDeltaTime;
+        float tempDeltaTime = currentTime - initialTime;
+        //const float realInitialTime = initialTime;
+
+        foreach(Transform t in thingsMoving)
+        {
+            times[thingsMoving.IndexOf(t)] = currentTime;
+        }
+
+        for (int i = 0; i < thingsMoving.Count; i++)
+        {
+            Transform t = thingsMoving[i];
+            if (t != null && times[i] <= speedBelt) //check if ball is stored and if the timer on it isn't maxxed
+            {
+                thingMovingCurrentPosition.x = Mathf.Lerp(thingsMovingInitialPosition[i].x, transformBelt.right.x + transformBelt.position.x, times[i] / speedBelt);
+                thingMovingCurrentPosition.y = Mathf.Lerp(thingsMovingInitialPosition[i].y, transformBelt.right.y + transformBelt.position.y, times[i] / speedBelt);
+
+                thingsMoving[i].position = new Vector3(thingMovingCurrentPosition.x, thingMovingCurrentPosition.y, 0f);
+
+                times[i]++;
+            }
+            else
+            {
+                thingsMoving[i] = null;
+                thingsMovingInitialPosition[i] = Vector3.zero;
+                times[i] = 0;
+            }
+        }
+        return null;
+    }
+
     public void TurnMove()
     {
         Debug.Log("Belt moves ball");
+        moveThing();
+
+
     }
 
     public void TurnEffect() //Belts have no effect
     {
         Debug.Log("Belt effect (none)");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) // detect ball on belt //do something so it doesnt fuck up with multiple balls on same belt
+    {
+        if (collision.CompareTag("Ball"))
+        {
+            collision.transform.rotation = transformBelt.rotation; //rotate ball to be same facing as the belt
+            for (int i = 0; i < thingsMoving.Count; i++)
+            {
+                Transform t = thingsMoving[i];
+                if (t == null)//if found empty spot change bool to yes and set it to that spot and reset the timer attached at same index and get the initial position of it
+                {
+                    thingsMoving[i] = collision.transform;
+                    thingsMovingInitialPosition[i] = collision.transform.position;
+                    times[i] = 0;
+                    spotfound = true;
+                    break;//breaks when it finds a free space
+                }
+            }
+
+            if (!spotfound)//if no spot add it to the end and add another time var  
+            {
+                thingsMoving.Add(collision.transform);
+                thingsMovingInitialPosition.Add(collision.transform.position);
+                times.Add(0);
+            }
+
+            spotfound = false; //reset to default
+        }
     }
 
 }
