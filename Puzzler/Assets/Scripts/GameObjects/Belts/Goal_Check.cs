3facc86f63f78464afdd84d8517998df0094366d
Alvin-Tulud <alvintulud@gmail.com>
2024-01-13 14:44:52 -0800

    upd8 goal check

--- Puzzler/Assets/Scripts/GameObjects/Belts/Goal_Check.cs
+++ Puzzler/Assets/Scripts/GameObjects/Belts/Goal_Check.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Goal_Check : MonoBehaviour
+public class Goal_Check : MonoBehaviour, Tile_Interface
 {
     public List<string> Goal_Colors = new List<string>(4);
+
+    public void TurnMove()
+    {
+        Debug.Log("do nothing");
+    }
+
+    public void TurnEffect() //Belts have no effect
+    {
+        Debug.Log("do nothing");
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
