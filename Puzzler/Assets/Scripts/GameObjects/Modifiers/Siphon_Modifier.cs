802ffd4cd32b70c87658261a6bf312f4bc015050
Alvin-Tulud <alvintulud@gmail.com>
2024-01-16 23:02:21 -0800

    siphon added

---
+++ Puzzler/Assets/Scripts/GameObjects/Modifiers/Siphon_Modifier.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Siphon_Modifier : Tile_Movement_Parent, Tile_Interface
+{
+    void Start()
+    {
+        transformBelt = this.gameObject.GetComponent<Transform>();
+
+        thingsMoving = new List<Transform>();
+        thingsMovingInitialPosition = new List<Vector3>();
+
+        spotfound = false;
+    }
+
+    public IEnumerator siphonLiquid()
+    {//for each ball check if theres a liquid in it then check if there is take out the first layer that it finds
+        foreach(Transform t in thingsMoving)
+        {
+
+        }
+        yield return null;
+    }
+
+    public void TurnEffect()
+    {
+        StartCoroutine(siphonLiquid());
+    }
+
+    public void TurnMove()
+    {
+        StartCoroutine(moveThing());
+    }
+}
