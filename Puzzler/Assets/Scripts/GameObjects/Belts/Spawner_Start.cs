13ae2657942e5d8131f471a376362fb6d2e07f80
Alvin-Tulud <alvintulud@gmail.com>
2024-01-17 22:06:17 -0800

    spawner finished

--- Puzzler/Assets/Scripts/GameObjects/Belts/Spawner_Start.cs
+++ Puzzler/Assets/Scripts/GameObjects/Belts/Spawner_Start.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner_Start : MonoBehaviour
+public class Spawner_Start : Tile_Movement_Parent, Tile_Interface
 {
-    // Start is called before the first frame update
+    public GameObject Flask_To_Spawn;
+    private bool isSpawned;
+
     void Start()
     {
-        
+        transformBelt = this.gameObject.GetComponent<Transform>();
+
+        thingsMoving = new List<Transform>();
+        thingsMovingInitialPosition = new List<Vector3>();
+
+        spotfound = false;
+
+        isSpawned = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TurnMove()
     {
-        
+        Debug.Log("Belt moves ball");
+        StartCoroutine(moveThing());
+    }
+    public void TurnEffect()
+    {
+        if (!isSpawned)
+        {
+            Instantiate(Flask_To_Spawn,transform.position,transform.rotation);
+            isSpawned = true;
+        }
     }
 }
