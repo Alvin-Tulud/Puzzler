44976934d48e769a1e03f3d0032c06aa382f45de
alee84sf <alee84@sfsu.edu>
2024-02-01 12:16:29 -0800

    starting work on goal visualizer script

--- Puzzler/Assets/Scripts/Control_Center/Menu&UI/Goal_Visualizer.cs
+++ Puzzler/Assets/Scripts/Control_Center/Menu&UI/Goal_Visualizer.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Goal_Visualizer : MonoBehaviour
 {
+    private Goal_End goal_end;
+    public List<string> Goal_Colors = new List<string>(4);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        goal_end = gameObject.GetComponent<Goal_End>();
+        Goal_Colors = goal_end.getGoalColors();
     }
 
     // Update is called once per frame
