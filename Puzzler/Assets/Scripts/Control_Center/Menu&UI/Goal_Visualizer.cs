f5fcf359b288daf8a20fdd75b8deb0fa5e5eba20
alee84sf <alee84@sfsu.edu>
2024-02-03 01:20:09 -0800

    goal clicker works now

--- Puzzler/Assets/Scripts/Control_Center/Menu&UI/Goal_Visualizer.cs
+++ Puzzler/Assets/Scripts/Control_Center/Menu&UI/Goal_Visualizer.cs
@@ -9,6 +9,9 @@ public class Goal_Visualizer : MonoBehaviour
     private GameObject Display_Flask;
     private Ball_Modify Flask_Script;
 
+    private Collider2D Clickable_Space;
+    private LayerMask Clicker_Mask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +20,27 @@ public class Goal_Visualizer : MonoBehaviour
         GameObject g = GameObject.FindWithTag("Ball_Menu");
         Display_Flask = g.transform.GetChild(0).gameObject;
         Flask_Script = Display_Flask.GetComponent<Ball_Modify>();
+
+        Clickable_Space = gameObject.transform.Find("GoalClicker").GetComponent<Collider2D>(); //collider for clicking on goal
+        Clicker_Mask = 1 << 10;
+
+        Debug.Log(Clickable_Space);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Get the mouse position
+
         if (Input.GetMouseButtonDown(0)) //on click down
         {
-            Flask_Script.setColorModList(Goal_Colors);
-            Debug.Log(Goal_Colors[0] + Goal_Colors[1]);
+            if (Clickable_Space == Physics2D.OverlapPoint(mousePos, Clicker_Mask))//figure out how to make it so the tiles cant be placed on eachother
+            {
+                Flask_Script.setColorModList(Goal_Colors);
+                Debug.Log(Goal_Colors[0] + Goal_Colors[1]);
+            }
+
+                
 
             
         }
