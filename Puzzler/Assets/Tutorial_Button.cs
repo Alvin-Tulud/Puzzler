159fe870251f2b2e51568a49afe2291c01b8d9fb
Alvin-Tulud <alvintulud@gmail.com>
2024-02-02 21:48:45 -0800

    animator done

--- Puzzler/Assets/Tutorial_Button.cs
+++ Puzzler/Assets/Tutorial_Button.cs
@@ -7,28 +7,29 @@ public class Tutorial_Button : MonoBehaviour
     private RectTransform transform;
     public GameObject parent;
     public Animator animation;
-    public AnimationClip[] clips;
     private int i;
     // Start is called before the first frame update
     void Start()
     {
         transform = GetComponent<RectTransform>();
-        foreach(AnimationClip clip in clips)
-        {
-            
-        }
         i = 0;
     }
 
     public void next()
     {
         i++;
-        if (i < clips.Length)
+        if (i == 1)
         {
-
+            animation.SetBool("Drag->Rotate", true);
+        }
+        else if (i == 2)
+        {
+            animation.SetBool("Drag->Rotate", false);
+            animation.SetBool("Rotate->Flask", true);
         }
         else
         {
+            animation.SetBool("Rotate->Flask", false);
             Destroy(parent);
         }
     }
