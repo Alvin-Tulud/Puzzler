634a0304d696b8fc48bc61f8557f6676672753d8
Alvin-Tulud <alvintulud@gmail.com>
2024-02-03 01:39:49 -0800

    fucked up next button level 28 fix

--- Puzzler/Assets/Scripts/Control_Center/Menu&UI/Tutorial_Button.cs
+++ Puzzler/Assets/Scripts/Control_Center/Menu&UI/Tutorial_Button.cs
@@ -8,6 +8,7 @@ public class Tutorial_Button : MonoBehaviour
     public GameObject parent;
     public Animator animation;
     private int i;
+    private int previousrand;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +18,26 @@ public class Tutorial_Button : MonoBehaviour
 
     public void next()
     {
+        int rand = Random.Range(100, 1100);
+        while (Mathf.Abs(rand - previousrand) < 250)
+        {
+            rand = Random.Range(100, 1100);
+        }
+
         i++;
         if (i == 1)
         {
             animation.SetBool("Drag->Rotate", true);
+
+            transform.position = new Vector3(rand, transform.position.y, transform.position.z);
+            previousrand = rand;
         }
         else if (i == 2)
         {
             animation.SetBool("Drag->Rotate", false);
             animation.SetBool("Rotate->Flask", true);
+
+            transform.position = new Vector3(rand, transform.position.y, transform.position.z);
         }
         else
         {
